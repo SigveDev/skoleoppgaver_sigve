923f72d0f45fe18f814e7f1c0968c62eb9bf7506
Sigve <iam2derp4you@gmail.com>
2022-09-13 14:40:21 +0200

    databse på frontend

--- databaser/databasefrontend/hello-world.js
+++ databaser/databasefrontend/hello-world.js
@@ -1,9 +1,28 @@
+var mysql = require('mysql');
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    const con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "dromtorp"
+      });
+      
+      con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+
+        var sql = "select * from `elev` where Fornavn = 'Peder'";
+
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          //console.log(result);
+          res.send(result);
+        });
+      });
 })
 
 app.listen(port, () => {
