f6c388af53085cd3885d119ba24c233ab8eb1de7
2derp4you <iam2derp4yoy@gmail.com>
2022-12-20 13:32:48 +0100

    js oppgaver (uferdig)

--- JS-Oppgaver/oppgaver_13_Des/test.js
+++ JS-Oppgaver/oppgaver_13_Des/test.js
@@ -1,9 +1,27 @@
-function kvadrat(){
-    let areal = 8 * 8;
+function kvadrat(lengde, bredde){
+    let areal = lengde * bredde;
     console.log(areal);
 }
 
-function trekant(){
-    let areal = (8 * 8) / 2;
+function trekant(lengde, bredde){
+    let areal = (lengde * bredde) / 2;
     console.log(areal);
+}
+
+function areal(lengde, bredde){
+    kvadrat(lengde, bredde);
+    trekant(lengde, bredde);
+}
+
+function spraak(){
+    let lang = document.getElementById("input").value;
+    if (lang == "n"){
+        document.getElementById("output").innerHTML = "Du er Norsk";
+    } else if (lang == "s"){
+        document.getElementById("output").innerHTML = "Du er Svansk";
+    } else if (lang == "d"){
+        document.getElementById("output").innerHTML = "Du er Dansk";
+    } else {
+        document.getElementById("output").innerHTML = "Skriv inn n for Norge, s for Sverige eller d for Danmark";
+    }
 }
