a9b1ef4a84846a415462b7af0e144a7e995b0d79
2derp4you <iam2derp4you@gmail.com>
2023-03-28 23:17:47 +0200

    test

--- jimbro/src/components/getUser.js
+++ jimbro/src/components/getUser.js
@@ -3,27 +3,25 @@ import { useEffect, useState } from 'react';
 export function getUser() {
     const [user, setUser] = useState(null);
 
-    useEffect(()=>{
-        const getUser = ()=>{
-          fetch("https://api.jimbro.fyi/auth/login/success", {
-            method: "GET",
-            credentials: "include",
-            headers: {
-              Accept: "application/json",
-              "content-type": "application/json",
-              "Access-Control-Allow-Credentials": true,
-            },
-          }).then(response=>{
-            if (response.status === 200) return response.json();
-            throw new Error("authentication failed");
-          }).then(resObject=>{
-            setUser(resObject.user);
-          }).catch(err=>{
-            console.log(err);
-          });
-        };
-        getUser();
-      },[])
+    const getUser = ()=>{
+        fetch("https://api.jimbro.fyi/auth/login/success", {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            Accept: "application/json",
+            "content-type": "application/json",
+            "Access-Control-Allow-Credentials": true,
+          },
+        }).then(response=>{
+          if (response.status === 200) return response.json();
+          throw new Error("authentication failed");
+        }).then(resObject=>{
+          setUser(resObject.user);
+        }).catch(err=>{
+          console.log(err);
+        });
+      };
+      getUser();
 
     return user;
 }
